80c8eb9c57771d3bb2981379c387a5a17757be84
Your Name <you@example.com>
2017-08-19 12:37:19 +0800

    rainbow

---
+++ sinatra/rainbows.ru
@@ -0,0 +1,7 @@
+#
+
+worker_processes 2
+Rainbows! do
+  use :XEpollThreadPool, pool_size: 8
+  worker_connections 128
+end
---
+++ sinatra/rainbows.sh
@@ -0,0 +1,1 @@
+RACK_ENV=production DATABASE_URL='postgres://localhost/testdb?pool=5' bundle exec rainbows -c rainbows.ru -l /tmp/test.sock
