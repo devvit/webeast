3cc36f323700f77625f67862559eac60e68da157
Your Name <you@example.com>
2018-03-23 19:43:33 +0800

    aiohttp

--- aio/sanic_app.py
+++ aio/sanic_app.py
@@ -2,43 +2,18 @@
 
 import socket
 
-import aioredis
-import peewee
-import peewee_async
-
 from sanic import Sanic
 from sanic import response
 
+from db import init_redis, objs, Item
+
 app = Sanic(__name__, log_config=None)
 app.config['KEEP_ALIVE_TIMEOUT'] = 30
-db = peewee_async.PooledPostgresqlDatabase(
-    'testdb',
-    host='/tmp',
-    autocommit=True,
-    autorollback=True,
-    max_connections=10
-)
-db.set_allow_sync(False)
-objs = peewee_async.Manager(db)
-
-
-class Item(peewee.Model):
-    title = peewee.CharField()
-
-    class Meta:
-        database = db
-        db_table = 'items'
 
 
 @app.listener('before_server_start')
 async def before_server_start(app, loop):
-    app.redis_pool = await aioredis.create_redis_pool(
-        '/tmp/redis.sock',
-        encoding='utf-8',
-        minsize=1,
-        maxsize=10,
-        loop=loop
-    )
+    await init_redis(app, loop)
 
 
 @app.route('/json')
