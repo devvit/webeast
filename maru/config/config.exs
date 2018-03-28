71040c80dc6c883ad3e6cf7bb909789860947cb5
Your Name <you@example.com>
2018-03-28 22:29:14 +0800

    cowboy max_keepalive

--- maru/config/config.exs
+++ maru/config/config.exs
@@ -22,31 +22,10 @@ config :redis_pool, :pools, [
 
 config :memcachir, hosts: "localhost", pool: [size: 10, max_overflow: 10, strategy: :lifo]
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure for your application as:
-#
-#     config :myweb, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:myweb, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
-
-config :maru, Myweb.Apiv1, http: [port: 3000, ip: {0,0,0,0}]
+config :maru, Myweb.Apiv1, http: [
+  port: 3000,
+  ip: {0,0,0,0},
+  protocol_options: [
+    max_keepalive: 5_000_000
+  ]
+]
