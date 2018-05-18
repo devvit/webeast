207ead2b27cf2cb9bc7e774955635e546a4c1ced
devvit <you@example.com>
2018-05-18 10:11:04 +0800

    dep

--- go/dep.sh
+++ go/dep.sh
@@ -1,3 +1,6 @@
+export http_proxy=http://192.168.0.100:8123
+export https_proxy=http://192.168.0.100:8123
+
 go get github.com/gin-gonic/gin
 go get github.com/michelloworld/ez-gin-template
 go get github.com/go-xorm/xorm
@@ -5,9 +8,10 @@ go get github.com/lib/pq
 go get github.com/garyburd/redigo/redis
 go get github.com/parnurzeal/gorequest
 go get github.com/pangudashu/memcache
-go get -u github.com/valyala/fasthttp
+go get github.com/valyala/fasthttp
 go get github.com/buaazp/fasthttprouter
 go get github.com/gofury/fusion
 go get github.com/fasthttp-contrib/render
-go get -u github.com/go-pg/pg
-go get -u github.com/go-redis/redis
+go get github.com/go-pg/pg
+go get github.com/go-redis/redis
+go get github.com/qiangxue/fasthttp-routing
